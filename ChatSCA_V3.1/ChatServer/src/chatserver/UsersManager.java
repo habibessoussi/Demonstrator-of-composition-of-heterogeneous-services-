b3054ac0cfc532f1da7094f1117784f45dfa5808
Mohamed Habib ESSOUSSI <mohamedhabib.essoussi@gmail.com>
2012-02-08 13:11:32 +0100

    MIT sources and ressources

--- ChatSCA_V3.1/ChatServer/src/chatserver/UsersManager.java
+++ ChatSCA_V3.1/ChatServer/src/chatserver/UsersManager.java
@@ -1,3 +1,34 @@
+/*
+ * Master-Level project:                                                           
+ *       Demonstrator of composition of heteregenous services                      
+ *       with SCA                                                                 
+ *                                                                                 
+ *  Copyright (C) 2012                                                            
+ *  Authors: Mohamed Habib ESSOUSSI                                                
+ *           Mohamed Said MOSLI BOUSKIAA                                           
+ *                                                                                 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in    
+ * the Software without restriction, including without limitation the rights to     
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies    
+ * of the Software, and to permit persons to whom the Software is furnished to do   
+ * so, subject to the following conditions:                                         
+ *                                                                                  
+ * The above copyright notice and this permission notice shall be included in all   
+ * copies or substantial portions of the Software.                                  
+ *                                                                                  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    
+ * SOFTWARE.                                                                        
+ *                                                                                  
+ *  TELECOM SudParis | Oct 2011-Jan 2012                                            
+ */                                                                                  
+
+
 package chatserver;
 
 import org.osoa.sca.annotations.Remotable;
@@ -20,8 +51,9 @@ public interface UsersManager {
 	 * @param pseudo The pseudo of the user to be connected.
 	 * @param password The password of the user to be connected.
 	 * @return This method returns 1 if the user is successfully 
-	  * connected and 0 otherwise.
+	 * connected and 0 otherwise.
 	 */
+	 
 	int connect(String pseudo, String password);
 	
 	 /**
